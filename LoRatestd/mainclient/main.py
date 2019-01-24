bd6128ddd0e577b22f7b3283c4ea9b665760309b
SANCHEZJOSE <j.sanchez08@ufromail.cl>
2019-01-24 12:29:08 -0300

    update

--- LoRatestd/mainclient/main.py
+++ LoRatestd/mainclient/main.py
@@ -10,7 +10,8 @@ from controller_esp32 import ESP32Controller
 #{'frequency': 866E6, 'tx_power_level': 2, 'signal_bandwidth': 125E3,'spreading_factor': 8, 'coding_rate': 5, 'preamble_length': 8,'implicitHeader': False, 'sync_word': 0x12, 'enable_CRC': False}
 
 controller = ESP32Controller()
-lora = controller.add_transceiver(SX127x(name = 'LoRa1',parameters ={'frequency': 866E6, 'tx_power_level': 2, 'signal_bandwidth': 125E3,'spreading_factor': 8, 'coding_rate': 5, 'preamble_length': 8,'implicitHeader': False, 'sync_word': 0x12, 'enable_CRC': False} ),
+objeto=SX127x(name = 'LoRa1',parameters = {'frequency': 866E6, 'tx_power_level': 2, 'signal_bandwidth': 125E3,'spreading_factor': 8, 'coding_rate': 5, 'preamble_length': 8,'implicitHeader': False, 'sync_word': 0x12, 'enable_CRC': False})
+lora = controller.add_transceiver(objeto,
                                   pin_id_ss = ESP32Controller.PIN_ID_FOR_LORA_SS,
                                   pin_id_RxDone = ESP32Controller.PIN_ID_FOR_LORA_DIO0)
 

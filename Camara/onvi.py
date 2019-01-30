ef7f1bcd4a5e8b5d698c4e9c537250a46da7c777
Victor M Bustos <v.bustos01@ufromail.cl>
2019-01-30 15:46:07 -0300

    info profile de la camara

--- Camara/onvi.py
+++ Camara/onvi.py
@@ -1,5 +1,6 @@
 from onvif import ONVIFCamera
 
+# instanciacion del objeto onvif
 ip = '192.168.0.64'
 port = 80
 user = 'admin'
@@ -7,28 +8,36 @@ passwd = 'fondef18I10360'
 cam = ONVIFCamera(ip, port, user, passwd, '/home/vitocosmic/Escritorio/python-onvif/wsdl')
 
 # informacion de la camara
-print "Informacion de la camara:\n"
 info = cam.devicemgmt.GetHostname()
-print "nombre de la camara: " + str(info.Name) + "\n"
 fecha = cam.devicemgmt.GetSystemDateAndTime()
+usr = cam.devicemgmt.GetUsers()
+
+# prints
+print "Informacion de la camara:\n"
+print "nombre de la camara: " + str(info.Name) + "\n"
 print "zona horaria: " + str(fecha.TimeZone) + "\n"
 print "year: " + str(fecha.UTCDateTime.Date.Year) + "\n"
 print "hora: {}:{}:{}".format(str(fecha.UTCDateTime.Time.Hour),
 	fecha.UTCDateTime.Time.Minute, fecha.UTCDateTime.Time.Second) + "\n"
+########################################################################
+for i in usr:
+	print i
 
 # protocolos utilizados
 proto = cam.devicemgmt.GetNetworkProtocols()
 for i in proto:
 	print i
 
+# servicio media
+media_serv = cam.create_media_service()
+print media_serv.GetProfile()
+
 # PTZ
 ptz_serv = cam.create_ptz_service()
 # ejemplo funcional de libreria ptz
-pt = cam.ptz.GetServiceCapabilities()
-
-
+#pt = cam.ptz.GetServiceCapabilities()
 
 ############### revision de atributos #################
 ############ comentar de ser necesario ################
-#for i in dir(cam.ptz):
+#for i in dir(media_serv):
 #	print i

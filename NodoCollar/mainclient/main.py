59ff2625b1c102f4430c5fa1824d5f412fb06400
SANCHEZJOSE <j.sanchez08@ufromail.cl>
2019-02-01 11:41:30 -0300

    update

--- NodoCollar/mainclient/main.py
+++ NodoCollar/mainclient/main.py
@@ -1,7 +1,4 @@
-#import LoRaDuplexCallback
-#import LoRaPingPong
-import LoRaSender
-#import LoRaReceiver
+import Clientecollar
 import config_lora
 from sx127x import SX127x
 from controller_esp32 import ESP32Controller
@@ -15,7 +12,4 @@ lora = controller.add_transceiver(objeto,
                                   pin_id_ss = ESP32Controller.PIN_ID_FOR_LORA_SS,
                                   pin_id_RxDone = ESP32Controller.PIN_ID_FOR_LORA_DIO0)
 
-#LoRaDuplexCallback.duplexCallback(lora)
-#LoRaPingPong.ping_pong(lora)
-LoRaSender.send(lora)
-#LoRaReceiver.receive(lora)
+Clientecollar.collar(lora)

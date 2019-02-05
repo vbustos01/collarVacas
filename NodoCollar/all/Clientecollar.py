7dd27e50926ab32bb8688c8ad1c855323400d51b
SANCHEZJOSE <j.sanchez08@ufromail.cl>
2019-02-05 16:42:49 -0300

    NODO CENTRAL

--- NodoCollar/all/Clientecollar.py
+++ NodoCollar/all/Clientecollar.py
@@ -17,7 +17,7 @@ paqueteSync = False
 def collar(lora):
     print("LoRa Collar")
     lora.onReceive(on_receive)#Habilita la interrupcion del pin DIO0
-    lora.onTimeout(on_timeout,40)#Habilita la interrupcion del pin DIO1
+    lora.onTimeout(on_timeout,200)#Habilita la interrupcion del pin DIO1
     global display
     global paqueteActual
     paqueteActual = bytes([0,2]) + b'mensaje DE NODO PRUEBA 123456789' 

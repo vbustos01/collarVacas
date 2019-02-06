3381d523398544f07c05d551a80418f6a49350cd
SANCHEZJOSE <j.sanchez08@ufromail.cl>
2019-02-06 10:35:01 -0300

    update

--- Raspberry/SX127x/LoRa.py
+++ Raspberry/SX127x/LoRa.py
@@ -248,7 +248,7 @@ class LoRa(object):
         :rtype: bool
         """
         flags = self.get_irq_flags()
-        return not any([flags[s] for s in [not 'valid_header', 'crc_error']])
+        return not any([flags[s] for s in ['crc_error']])
         #return not any([flags[s] for s in ['valid_header', 'crc_error', 'rx_done', 'rx_timeout']])
 
     def read_payload(self , nocheck = False):

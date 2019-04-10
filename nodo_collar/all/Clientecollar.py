f7aa8ee69213da49bead243e746ba13ac8e7a578
SANCHEZJOSE <j.sanchez08@ufromail.cl>
2019-04-10 19:39:13 -0400

    lora2class

--- nodo_collar/all/Clientecollar.py
+++ nodo_collar/all/Clientecollar.py
@@ -1,109 +1,80 @@
-import ssd1306
-from machine import Pin, I2C, UART
 from direccionCollar import *
 from time import time
 from data_frame import *
+import config_lora
+from sx127x import SX127x
+from controller_esp32 import ESP32Controller
 
-vext = Pin(21, Pin.OUT)
-vext.value(0)
-rst = Pin(16, Pin.OUT)
-rst.value(1)
-scl = Pin(15, Pin.OUT, Pin.PULL_UP)
-sda = Pin(4, Pin.OUT, Pin.PULL_UP)
-i2c = I2C(scl=scl, sda=sda, freq=450000)
-display = ssd1306.SSD1306_I2C(128, 64, i2c, addr=0x3c)
+#{'frequency': 866E6, 'tx_power_level': 2, 'signal_bandwidth': 125E3,'spreading_factor': 8, 'coding_rate': 5, 'preamble_length': 8,'implicitHeader': False, 'sync_word': 0x12, 'enable_CRC': False}
 
-# inicializacion de GPS:
-uart = UART(2, 9600)
-uart.init(9600, bits=8, parity=None, stop=1,tx=17,rx=36) # se escogen dichos pines para no tener conflicto con oled
+class LoRa:
 
-paqueteEnviar = bytes(0)
-paqueteActual = bytes(0)
-intentosACK = 2
-intentos = 0
-paqueteSync = False
-SYMB_TIME_OUT = 200
+    def __init__(self,
+                 name = 'SX127x',
+                 parameters = {'frequency': 866E6, 'tx_power_level': 17, 'signal_bandwidth': 125E3,
+                               'spreading_factor': 8, 'coding_rate': 5, 'preamble_length': 8,
+                               'implicitHeader': False, 'sync_word': 0x12, 'enable_CRC': True},
+                 intentosACK=2,
+                 time_out_Symb = 200):
+        controller = ESP32Controller()
+        objeto=SX127x(name,parameters)
+        self.lora = controller.add_transceiver(objeto,
+                                          pin_id_ss = ESP32Controller.PIN_ID_FOR_LORA_SS,
+                                          pin_id_RxDone = ESP32Controller.PIN_ID_FOR_LORA_DIO0,
+                                          pin_id_RxTimeout= ESP32Controller.PIN_ID_FOR_LORA_DIO1)
+        self.paqueteEnviar = bytearray()
+        self.paqueteActual = bytearray()
+        self.intentosACK = 2
+        self.intentos = 0
+        self.paqueteSync = False
+        self.SYMB_TIME_OUT = time_out_Symb
 
+    def setup(self):
+        #print("LoRa Collar")
+        self.lora.onReceive(on_receive)#Asigna una función para la interrupcion del pin DIO0
+        self.lora.onTimeout(on_timeout,SYMB_TIME_OUT)#Asigna una función para la interrupcion del pin DIO1 y asigna un Timeout
+        # sensors = {'GPS':True,'IMU':False,'SD':True,'MIC':False}          
+        # pre_frame ={'address':255,'cmd':7,                                
+        #             'sensors':sensors,'location':"3844.7556,S,07236.9213,W", 
+        #             't_unix':454545666,'bateria':1024,'C_close':True}
+        self.lora.receive()
+        paqueteActual = empaquetar(pre_frame)
 
-def collar(lora):
-    print("LoRa Collar")
-    lora.onReceive(on_receive)#Asigna una función para la interrupcion del pin DIO0
-    lora.onTimeout(on_timeout,SYMB_TIME_OUT)#Asigna una función para la interrupcion del pin DIO1 y asigna un Timeout
-    global display
-    global paqueteActual
-    global led
-    sensors = {'GPS':True,'IMU':False,'SD':True,'MIC':False}          
-    pre_frame ={'address':255,'cmd':7,                                
-                'sensors':sensors,'location':"3844.7556,S,07236.9213,W", 
-                't_unix':454545666,'bateria':1024,'C_close':True}
-    display.fill(0)
-    display.text("LoRa Collar",0,0)
-    display.show()
-    lora.receive()
-    paqueteActual = empaquetar(pre_frame)
-    # Datos de Gps
-    """
-    while True:
-        try:
-            posicion = uart.readline()
-            if(posicion==None):
-                continue
-            posicion = posicion.decode("utf-8") # esto es equivalente a str() en py2
-            indicador = posicion.split(',')
-            if(indicador[0]=='$GPGGA'):
-                posicion=posicion[18:42]
-            if(indicador[0]=='$GPRMC'):
-                posicion=posicion[20:44]
-            pre_frame['location'] = posicion
-            print(pre_frame)
-        except:
-            continue
-    """
-
-def on_receive(lora,paquete):
-    global intentos
-    global display
-    global paqueteActual
-    global paqueteSync
-    global paqueteEnviar
-    display.fill(0)
-    if paquete:
-        direccion = paquete[0]
-        if direccion == dirCollar:
-            comando = paquete[1]
-            #mensaje = paquete[2:].decode()
-            display.text("Recibi:",0,10)
-            if comando == 0:
-                paqueteSync = True#llego un paquete de sincronización
-                lora.bytesprintln(paqueteActual)
-                paqueteEnviar = paqueteActual
-                display.text("Sync",0,20)
-                lora.receiveSingle()
-            elif paqueteSync and (comando == 1):
-                paqueteSync = False
-                display.text("ACK",0,20)
-                intentos = 0
-                lora.receive()
-        else:
-            display.text("Direccion Diferente",0,10)
-    else :
-        display.text("Paquete con Error",0,10)
-        lora.receive()
-    if paqueteSync:
-        lora.receiveSingle()
-    display.text("RSSI:{0}".format(lora.packetRssi()),0,30)
-    display.show()
-
-
-
-def on_timeout(lora):
-    global intentosACK
-    global paqueteEnviar
-    global intentos
-    intentos += 1
-    if intentos <= intentosACK:
+    def on_receive(self,paquete):
+        if paquete:
+            direccion = paquete[0]
+            if direccion == dirCollar:
+                comando = paquete[1]
+                #mensaje = paquete[2:].decode()
+                display.text("Recibi:",0,10)
+                if comando == 0:
+                    paqueteSync = True#llego un paquete de sincronización
+                    lora.bytesprintln(paqueteActual)
+                    paqueteEnviar = paqueteActual
+                    print("Sync")
+                    lora.receiveSingle()
+                elif paqueteSync and (comando == 1):
+                    paqueteSync = False
+                    print("ACK")
+                    intentos = 0
+                    lora.receive()
+            else:
+                print("Direccion Diferente")
+        else :
+            print("Paquete con Error")
+            lora.receive()
         if paqueteSync:
-            lora.bytesprintln(paqueteEnviar)
-            lora.receiveSingle()#se espera nuevamente un ACK
-    else:
-        lora.receive()#intentos no cumplidos
+            lora.receiveSingle()
+        print("RSSI:{0}".format(lora.packetRssi()))
+    
+    def on_timeout(lora):
+        global intentosACK
+        global paqueteEnviar
+        global intentos
+        intentos += 1
+        if intentos <= intentosACK:
+            if paqueteSync:
+                lora.bytesprintln(paqueteEnviar)
+                lora.receiveSingle()#se espera nuevamente un ACK
+        else:
+            lora.receive()#intentos no cumplidos

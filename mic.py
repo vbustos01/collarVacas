97a32733881ab2f3f351e02c50264039bf2bee43
DiegoPinillaU <d.pinilla03@ufromail.cl>
2019-04-25 16:55:48 -0400

    se separó la SD de la clase Mic, se debe usar el método attachSD para añadir una SD

--- mic.py
+++ mic.py
@@ -1,6 +1,6 @@
 from machine import I2S, Pin, SPI
 from drivers import sdcard
-import utime, uos
+import utime, uos, random
 
 class Mic(object):
 	"""docstring for Mic"""
@@ -14,46 +14,38 @@ class Mic(object):
 
 	def __init__(self):
 		super(Mic, self).__init__()
-
-		
+		self.sd = None
+		self.audio = None
 		self.vext_pin = Pin(21, Pin.OUT, value=0)
 		self.cs_lora = Pin(18,Pin.OUT, value=1)
 		self.initMic()
-
-		"""
-		self.audio = I2S(I2S.NUM0, bck=bck_pin, ws=ws_pin, sdin=sdin_pin, 
-              standard=I2S.PHILIPS, mode=I2S.MASTER_RX,
-              dataformat=I2S.B32, channelformat=I2S.ONLY_RIGHT,
-              samplerate=Mic.SAMPLES_PER_SECOND*2,
-              dmacount=8, dmalen=256)
-		"""
-		self.spi = SPI(sck=Pin(23), mosi=Pin(12), miso=Pin(14))
+		
 	"""
 	
 	"""
 	def sleep(self, state):
 		if state is True:
-			self.audio.deinit()
+			self.audio.deinit() if self.audio is not None
 			#self.cs_lora.value(1)
 		else:
-			self.initMic()
+			self.vext_pin.value(1)
+			self.vext_pin.value(0)
+			self.initMic() 
 			#self.cs_lora.value(0)
 
 	def record(self, duration = 4):
-		self.sleep(False)
-		WAV_DATA_SIZE = duration * Mic.SAMPLES_PER_SECOND * Mic.BYTES_PER_SAMPLE
-		wav_header = self.gen_wav_header(Mic.SAMPLES_PER_SECOND, Mic.BITS_PER_SAMPLE, 1, Mic.SAMPLES_PER_SECOND * duration)
-		if self.initSD() is True:
-			f = open('/sd/mic_recording.wav','wb')
+		if self.sd is not None:
+			self.sleep(False)
+			WAV_DATA_SIZE = duration * Mic.SAMPLES_PER_SECOND * Mic.BYTES_PER_SAMPLE
+			wav_header = self.gen_wav_header(Mic.SAMPLES_PER_SECOND, Mic.BITS_PER_SAMPLE, 1, Mic.SAMPLES_PER_SECOND * duration)
+			uos.mount(self.sd, "/")
+			f = open('/mic_recording_'+str(random.randint(1,1000))+'.wav','wb')
 			f.write(wav_header)
-
 			now = utime.ticks_ms()
-
 			mic_samples = bytearray(Mic.SAMPLE_BLOCK_SIZE)
 			sd_samples = bytearray(Mic.SDCARD_SECTOR_SIZE)
 			numread = 0
 			numwrite = 0
-
 			for _ in range(WAV_DATA_SIZE // Mic.SDCARD_SECTOR_SIZE):
 			    try:
 			        numread += self.audio.readinto(mic_samples)    
@@ -61,29 +53,19 @@ class Mic(object):
 			        numwrite += f.write(sd_samples)
 			    except KeyboardInterrupt:  
 			        break
+			elapsedTime = utime.ticks_diff(utime.ticks_ms(),now)
 			f.close()
-			uos.umount("/sd")
-			self.spi.deinit()
+			uos.umount("/")
 			self.sleep(True)
-			print("Listo, bytes leídos: {}, bytes escritos: {}, duración: {}".format(numread,
-																				numwrite,
-																				utime.ticks_diff(utime.ticks_ms(),now)))
+			print("Listo, bytes leídos: {}, bytes escritos: {}, \nduración segun ticks: {}, duración segun calculos {}".format(numread,
+																					numwrite,
+																					elapsedTime,
+																					numwrite/8000.0))
 		else:
-			print("Fallo la grabación")
+			print("Por favor, añade una sd")
 
-	def initSD(self):
-		counter = 0
-		while counter < 3:
-			try:
-			    sd = sdcard.SDCard(self.spi, Pin(2,Pin.OUT))
-			    vfs =uos.VfsFat(sd)
-			    uos.mount(vfs, "/sd")
-			    return True
-			except Exception as e:
-			    print("Error al montar sd, intento: "+str(counter))
-			    self.spi.deinit()
-			counter += 1
-		return False
+	def attachSD(self, sd):
+		self.sd = sd
 
 	def initMic(self):
 		try:

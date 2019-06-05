1e419f19730a0edde42a255fc06b86db16065fb8
DiegoPinillaU <d.pinilla03@ufromail.cl>
2019-06-05 17:10:05 -0400

    se añade timeout al logger del gps, cambio al nombre de la clase

--- clases/gps.py
+++ clases/gps.py
@@ -3,23 +3,24 @@
 # ademas inicializa la comunicacion via UART con el gps
 
 from machine import UART
-from time import sleep
+from time import sleep, ticks_diff, ticks_ms
 from uos import *
 
-class Gps_upy(UART):
+class GPS():
 	def __init__(self):
-		super(Gps_upy, self).__init__(2, 9600)
-		super().init(9600, bits=8, parity=None, stop=2, tx=17, rx=33)
+		self.uart = UART(2, 9600)
+		self.uart.init(9600, bits=8, parity=None, stop=2, tx=17, rx=33)
+		self.pos = 0
 		# configurar gps a 9600 baud
-		#super().write(b'$PMTK251,9600*27\r\n')
+		#self.uart.write(b'$PMTK251,9600*27\r\n')
 
-	def write2sd(self):
+	def write2sd(self, timeout):
 		# Metodo utilizado para guardar la posicion actual en la sd
-		super().write(b'$PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29\r\n')
+		self.uart.write(b'$PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29\r\n')
 		sleep(1)
-
-		while 1:
-			self.frame = super().readline()
+		timestart = ticks_ms()
+		while ticks_diff(ticks_ms(), timestart) < timeout:
+			self.frame = self.uart.readline()
 			self.pos = str(self.frame).split(',')
 			try:
 				if self.pos[2]=='A':
@@ -29,9 +30,22 @@ class Gps_upy(UART):
 			except:
 				print('error desconocido')
 
+		if ticks_diff(ticks_ms(), timestart) >= timeout:
+			print("timeout! : "+str(timeout)+" ms")
+			if self.sd is not None:
+				mount(self.sd, "/")
+				filename = '/gps_data.txt'
+				with open(filename,'a') as f:
+					n = f.write("posición no determinada\n")
+					f.close()
+				umount("/")
+			else:
+				print('sd no detectada')
+			return
+
 		#self.pack = self.pos[3:7]
-		self.pack = self.pos[3]+','+self.pos[4]+','+self.pos[5]+','+self.pos[6]
 		print('data a escribir(GPS):')
+		self.pack = self.pos[3]+','+self.pos[4]+','+self.pos[5]+','+self.pos[6]
 		print(self.pack)
 		# Escritura en tarjeta SD
 		if self.sd is not None:
@@ -39,16 +53,17 @@ class Gps_upy(UART):
 			filename = '/gps_data.txt'
 			with open(filename,'a') as f:
 				n = f.write('{}\n'.format(self.pack))
+				f.close()
 			umount("/")
 		else:
 			print('sd no detectada')
 
 	def req_pos():
-		super().write(b'$PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29\r\n')
+		self.uart.write(b'$PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29\r\n')
 		sleep(1)
 
 		while 1:
-			self.frame = super().readline()
+			self.frame = self.uart.readline()
 			self.pos = str(self.frame).split(',')
 			try:
 				if self.pos[2]=='A':
@@ -79,55 +94,55 @@ class Gps_upy(UART):
 	def cold_start(self):
 		# modo por defecto del gps
 		# busca almanaque
-		super().write(b'$PMTK103*30\r\n')
+		self.uart.write(b'$PMTK103*30\r\n')
 	def full_cold_start(self):
-		super().write(b'$PMTK104*37\r\n')
+		self.uart.write(b'$PMTK104*37\r\n')
 	def warm_start(self):
 		# obtiene efemerides
-		super().write(b'$PMTK102*31\r\n')
+		self.uart.write(b'$PMTK102*31\r\n')
 	def hot_start(self):
 		# hot start
-		super().write(b'$PMTK101*32\r\n')
+		self.uart.write(b'$PMTK101*32\r\n')
 
 	# Modos de energia
 	def standby_mode(self):
-		super().write(b'$PMTK161,0*28\r\n')
+		self.uart.write(b'$PMTK161,0*28\r\n')
 	def sleep_mode(self):
-		super().write(b'$PMTK161,1*29\r\n')
+		self.uart.write(b'$PMTK161,1*29\r\n')
 	def periodic_mode(self):
 		# ejemplo del data
-		#super().write(b'$PMTK225,2,3000,12000,18000,72000*15\r\n')
+		#self.uart.write(b'$PMTK225,2,3000,12000,18000,72000*15\r\n')
 
 		# my setup (7 segundos activo, 1 min dormido)
-		#super().write(b'$PMTK225,2,7000,60000,0,0*18\r\n')
+		#self.uart.write(b'$PMTK225,2,7000,60000,0,0*18\r\n')
 
 		# my setup 2.0 (7 seg on 14 seg off)
-		#super().write(b'$PMTK225,8,7000,14000,0,0*11\r\n')
+		#self.uart.write(b'$PMTK225,8,7000,14000,0,0*11\r\n')
 
 		# setup 3 (5 min on 15 min off)
-		super().write(b'$PMTK225,2,180000,900000,0,0*29\r\n')
+		self.uart.write(b'$PMTK225,2,180000,900000,0,0*29\r\n')
 
 
 	def attachSD(self, sd):
 		self.sd = sd
 	def nmea_out(self):
 		# solo GPRMC
-		super().write(b'$PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29\r\n')
+		self.uart.write(b'$PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29\r\n')
 		# resetear por defecto
-		#super().write(b'$PMTK314,-1*04\r\n')
+		#self.uart.write(b'$PMTK314,-1*04\r\n')
 
 	# Modos sin testear
 	def position_fix_interval(self):
 		# este parametro controla la tasa de obtencion de posicion de gps (position fix freq)
-		super().write(b'$PMTK500,1000,0,0,0,0*1A\r\n')
+		self.uart.write(b'$PMTK500,1000,0,0,0,0*1A\r\n')
 
 	def kill_data(self):
-		super().write(b'$PMTK120*31\r\n')
+		self.uart.write(b'$PMTK120*31\r\n')
 
 	def request_efemerides(self):
 		# busca en las efemerides 1800 seg atras
-		super().write(b'$PMTK660,1800*17\r\n');
+		self.uart.write(b'$PMTK660,1800*17\r\n');
 		#sleep()
-		#return super().read()
+		#return self.uart.read()
 
-#		super().write(b'')
+#		self.uart.write(b'')
